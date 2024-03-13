fc6bbd6916b4a5787a2d97f6f2824fc4a1cf18f0
xbzxit <xbzxit@163.com>
2024-03-13 14:53:31 +0800

    项目初始化-聚合工程

---
+++ README.md
@@ -0,0 +1,10 @@
+# 天天吃货
+> foodie-api
+> 
+> foodie-common
+> 
+> foodie-mapper
+> 
+> foodie-pojo
+> 
+> foodie-service
---
+++ foodie-api/src/main/java/com/xbzxit/foodie/FoodieApplication.java
@@ -0,0 +1,20 @@
+package com.xbzxit.foodie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HFZJ
+ * @version 1.0
+ * @create 2024-03-13-13:56
+ * @company www.nuzarsurf.com
+ */
+
+@SpringBootApplication
+public class FoodieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodieApplication.class, args);
+    }
+
+}

81663130feeb342562659ebecc91671acd40be84
xbzxit <xbzxit@163.com>
2024-03-18 15:58:15 +0800

    整个swagger2 完善

--- foodie-api/src/main/java/com/xbzxit/foodie/config/Swagger2.java
+++ foodie-api/src/main/java/com/xbzxit/foodie/config/Swagger2.java
@@ -1,5 +1,6 @@
 package com.xbzxit.foodie.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -21,6 +22,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class Swagger2 {
 
+    @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
@@ -32,13 +34,16 @@ public class Swagger2 {
 
     public ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("天天吃货 电商平台接口api")        // 文档页标题
-                .contact(new Contact("xbzxit",
-                        "https://xbzxit.com",
-                        "xbzxit@163.com"))        // 联系人信息
-                .description("专为天天吃货提供的api文档")  // 详细信息
-                .version("1.0.1")   // 文档版本号
-                .termsOfServiceUrl("https://www.xbzxit.com") // 网站地址
+                // 文档页标题
+                .title("天天吃货 电商平台接口api")
+                // 联系人信息
+                .contact(new Contact("xbzxit", "https://xbzxit.com", "xbzxit@163.com"))
+                // 详细信息
+                .description("天天吃货 接口文档 可以用于前后端联调的标准，也可以在线测试功能是否通过")
+                // 文档版本号
+                .version("1.0.1")
+                // 网站地址
+                .termsOfServiceUrl("https://www.xbzxit.com")
                 .build();
     }
 }
